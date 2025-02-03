7cdc9c6ef2a83d0e9f2525163177373ffc0366a1
tobyrushton <tobyjrushton@icloud.com>
2025-02-03 15:42:58 +0000

    created full scrape

--- packages/scrape/scrape.go
+++ packages/scrape/scrape.go
@@ -1,7 +1,66 @@
 package scrape
 
+import (
+	"sort"
+)
+
+type Player struct {
+	Id        int
+	FirstName string
+	LastName  string
+	Rating    int
+	NBAId     int
+}
+
 type Scraper struct{}
 
 func New() *Scraper {
 	return &Scraper{}
+}
+
+func (s *Scraper) Scrape() []Player {
+	webData := NewWebScraper().Scrape()
+	bdlData := NewBDLScraper().Scrape()
+
+	sort.Slice(webData, func(i, j int) bool {
+		if webData[i].FirstName == webData[j].FirstName {
+			return webData[i].LastName < webData[j].LastName
+		}
+		return webData[i].FirstName < webData[j].FirstName
+	})
+
+	sort.Slice(bdlData, func(i, j int) bool {
+		if bdlData[i].FirstName == bdlData[j].FirstName {
+			return bdlData[i].LastName < bdlData[j].LastName
+		}
+		return bdlData[i].FirstName < bdlData[j].FirstName
+	})
+
+	// now we match them and create new
+	data := make([]Player, 0)
+	i, j := 0, 0
+	for i < len(webData) && j < len(bdlData) {
+		if webData[i].FirstName == bdlData[j].FirstName {
+			if webData[i].LastName == bdlData[j].LastName {
+				data = append(data, Player{
+					Id:        bdlData[j].Id,
+					FirstName: bdlData[j].FirstName,
+					LastName:  bdlData[j].LastName,
+					NBAId:     webData[i].Id,
+				})
+				i++
+				j++
+			} else if webData[i].LastName < bdlData[j].LastName {
+				i++
+			} else {
+				j++
+			}
+		} else if webData[i].FirstName < bdlData[j].FirstName {
+			i++
+		} else {
+			j++
+		}
+	}
+
+	return data
 }
--- packages/scrape/scrape_test.go
+++ packages/scrape/scrape_test.go
@@ -6,6 +6,7 @@ import (
 	"testing"
 
 	"github.com/joho/godotenv"
+	"github.com/tobyrushton/elohoops/packages/scrape"
 )
 
 func TestMain(m *testing.M) {
@@ -15,4 +16,28 @@ func TestMain(m *testing.M) {
 	}
 
 	os.Exit(m.Run())
+}
+
+func TestScrape(t *testing.T) {
+	s := scrape.New()
+
+	players := s.Scrape()
+
+	for i, player := range players {
+		if player.FirstName == "" {
+			t.Errorf("Player %d has an empty FirstName", i)
+		}
+
+		if player.LastName == "" {
+			t.Errorf("Player %d has an empty LastName", i)
+		}
+
+		if player.Id == 0 {
+			t.Errorf("Player %d has an invalid Id (zero)", i)
+		}
+
+		if player.NBAId == 0 {
+			t.Errorf("Player %d has an invalid NBA Id (zero)", i)
+		}
+	}
 }
