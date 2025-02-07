9dae3b8d18f6ea60c09a96f8e7a0099071425f79
tobyrushton <tobyjrushton@icloud.com>
2025-02-07 22:52:28 +0000

    create navbar

---
+++ packages/frontend/src/components/logo.tsx
@@ -0,0 +1,10 @@
+import { FC } from "react"
+
+export const Logo: FC = () => {
+    return (
+        <h1 className="flex font-bold">
+            elo
+            <p className="text-orange-600">hoops</p>
+        </h1>
+    )
+}
---
+++ packages/frontend/src/components/navbar.tsx
@@ -0,0 +1,34 @@
+import { FC, PropsWithChildren } from "react"
+import { NavigationMenu, NavigationMenuItem, NavigationMenuList, NavigationMenuLink, navigationMenuTriggerStyle } from "./ui/navigation-menu"
+import { Logo } from "./logo"
+import Link from "next/link"
+import { Separator } from "./ui/separator"
+
+const NavItem: FC<PropsWithChildren<{ href: string }>> = ({ href, children }) => {
+    return (
+        <NavigationMenuItem>
+            <Link href={href} legacyBehavior passHref>
+                <NavigationMenuLink className={navigationMenuTriggerStyle()}>
+                    {children}
+                </NavigationMenuLink>
+            </Link>
+        </NavigationMenuItem>
+    )
+}
+
+export const Navbar: FC = () => {
+    return (
+        <NavigationMenu className="p-2 flex gap-1 h-10 items-center">
+            <Logo />
+            <Separator orientation="vertical" />
+            <NavigationMenuList>
+                <NavItem href="/">
+                    Home
+                </NavItem>
+                <NavItem href="/leaderboard">
+                    Leaderboard
+                </NavItem>
+            </NavigationMenuList>
+        </NavigationMenu>
+    )
+}
